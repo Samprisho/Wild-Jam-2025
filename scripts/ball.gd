39b0ef9f2524b8db66c1748f5b1e438e27557b5a
Samprisho <samuel.esho@protonmail.com>
2025-09-12 16:40:03 -0500

    Ball Movement

--- scripts/ball.gd
+++ scripts/ball.gd
@@ -1,15 +1,24 @@
 extends RigidBody3D
 class_name Ball
 
-@export var accelaration: float = 70
+@export var accelaration: float = 200
+@export var deaccelaration: float = 2
 
 func _physics_process(_delta: float) -> void:
 	var input = Vector2(
 		int(Input.is_action_pressed("Forward")) - int(Input.is_action_pressed("Backward")),
 		int(Input.is_action_pressed("Right")) - int(Input.is_action_pressed("Left"))
-	)
+	).normalized()
+
+	angular_velocity /= deaccelaration
 	
 	var calculated_velocity = calculate_motion(input)
+
+	calculated_velocity *= 5 if \
+	(calculated_velocity.normalized()).distance_to(angular_velocity.normalized()) > 2 else 1
+
+	print(calculated_velocity.normalized(), " | ", angular_velocity.normalized(), " | ", \
+	(calculated_velocity.normalized()).distance_to(angular_velocity.normalized()))
 	
 	angular_velocity += calculated_velocity
 
