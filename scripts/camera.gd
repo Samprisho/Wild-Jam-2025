700bc81dfb94b9d3a7e02632a45d45ffc061cb8e
Samprisho <samuel.esho@protonmail.com>
2025-09-27 13:05:02 -0500

    Better cam controls, added basic shader to ball to indicate direction, made basic obstacle in the map, and created physical materials for ball and course

--- scripts/camera.gd
+++ scripts/camera.gd
@@ -10,15 +10,21 @@ class_name CameraControls
 @export var targetNode: Node3D
 
 ## Controls how sensitive mouse motion is
-@export var mouseSensitivity: float = 10
+@export var mouseSensitivity: float = 35
 @export var sensitivityCurve: Curve
 @export var followSpeed: float = 7
+@export var rotSpeed: float = 15
+
+var targetRot: Vector3 = Vector3()
 
 func _ready() -> void:
 	Input.mouse_mode = Input.MOUSE_MODE_CAPTURED
+	targetRot = springArm.global_rotation
 
 func _input(event: InputEvent) -> void:
 	if event is InputEventMouseMotion:
+		var currRot = springArm.global_rotation
+		
 		var mouseMotion: InputEventMouseMotion = event
 		var velocity = mouseMotion.velocity * -1
 
@@ -26,8 +32,10 @@ func _input(event: InputEvent) -> void:
 			sensitivityCurve.sample(abs(velocity.x)),
 			sensitivityCurve.sample(abs(velocity.y))
 		) * sign(velocity))
-
+		
 		springArm.global_rotate(Vector3.UP, result.x)
+		targetRot = springArm.global_rotation
+		springArm.global_rotation = currRot
 
 func _process(delta: float) -> void:
 	springArm.global_position = lerp(
@@ -35,3 +43,8 @@ func _process(delta: float) -> void:
 		targetNode.global_position,
 		delta * followSpeed
 	)
+	springArm.global_rotation = Vector3(
+			lerp_angle(springArm.global_rotation.x, targetRot.x, delta * rotSpeed),
+			lerp_angle(springArm.global_rotation.y, targetRot.y, delta * rotSpeed),
+			lerp_angle(springArm.global_rotation.z, targetRot.z, delta * rotSpeed)
+		)
