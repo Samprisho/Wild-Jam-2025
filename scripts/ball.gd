cb725cd65d73bed5f9dad79213f5baddc6c8aee7
Samprisho <samuel.esho@protonmail.com>
2025-09-29 16:32:03 -0500

    better ball physics and shader

--- scripts/ball.gd
+++ scripts/ball.gd
@@ -2,7 +2,9 @@ extends RigidBody3D
 class_name Ball
 
 @export var accelaration: float = 200
-@export var deaccelaration: float = 1.4
+@export var deaccelaration: float = 1.05
+@export var brakeVelocityMultiplyer: float = 5
+@export var brakeVelocityMultiplyerThreshold: float = 0.9
 @export var springArm: Node3D
 
 func _physics_process(_delta: float) -> void:
@@ -14,14 +16,21 @@ func _physics_process(_delta: float) -> void:
 	angular_velocity /= deaccelaration
 	
 	var calculated_velocity = calculate_motion(input)
-
-	calculated_velocity *= 5 if \
-	(calculated_velocity.normalized()).distance_to(angular_velocity.normalized()) > 2 else 1
+	var actualDirection = Vector2(angular_velocity.x, angular_velocity.z).normalized()
+	var desiredDirection = Vector2(calculate_desired_rotation_direction(input).x, calculate_desired_rotation_direction(input).z).normalized()
 	
-	angular_velocity += calculated_velocity
+	var difference = abs(desiredDirection.angle() - actualDirection.angle())
+	
+	
+	angular_velocity += calculated_velocity * (brakeVelocityMultiplyer if difference >= 1 else 1)
 
 func calculate_motion(input):
-	var dir = ((springArm.global_basis.x * input.x) + (springArm.global_basis.z * input.y)).normalized()
+	var dir = calculate_desired_rotation_direction(input)
 	
 	return get_physics_process_delta_time() * accelaration * dir
-	
+
+func calculate_desired_rotation_direction(input: Vector2):
+	var dir = ((springArm.global_basis.x.normalized() * input.x) + (springArm.global_basis.z.normalized() * input.y)).normalized()
+	return dir
+func calculate_desired_linear_direction(input: Vector2): 
+	pass
