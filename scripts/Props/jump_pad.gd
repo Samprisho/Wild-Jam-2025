02630017c7663077dd02c3fbe5889d76ecdb34fe
Samprisho <samuel.esho@protonmail.com>
2026-02-20 23:18:20 -0600

    More gym, more mp_01, and better glass shader.
    Maybe I should rename the glass material to goal material, since it
    sticks out and could be used to grab the player's attention to something
    important...🤔

--- scripts/Props/jump_pad.gd
+++ scripts/Props/jump_pad.gd
@@ -8,14 +8,29 @@ class_name JumpPad
 
 @export var detactionArea: Area3D
 @export var launchDirectionNode: Marker3D
+@export var mesh: MeshInstance3D
 
 @export_category("Launch properties")
 @export var jumpPadPower: float = 13
 @export var velocityOverride: bool = true
 
+var mat: StandardMaterial3D
+
 func _ready() -> void:
 	super()
 	detactionArea.body_entered.connect(_on_body_detected)
+	
+	if mesh.material_override is StandardMaterial3D:
+		mat = mesh.material_override
+	
+	prop_activate.connect(func ():
+		mat.albedo_color = Color.SKY_BLUE
+	)
+	
+	prop_deactivate.connect(func ():
+		mat.albedo_color = Color.GRAY
+	)
+	
 
 func _on_body_detected(body: Node3D):
 	if not fulfilled:
